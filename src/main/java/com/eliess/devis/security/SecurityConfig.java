d26db8dc4220ec1da568e8f24ef0f2bc2e4d4397
eliess <eloykreir85@gmail.com>
2026-05-18 11:20:01 +0200

    Ajout Cors Config

--- src/main/java/com/eliess/devis/security/SecurityConfig.java
+++ src/main/java/com/eliess/devis/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.eliess.devis.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
         http
+        .cors(Customizer.withDefaults()) // ajout cors config
         // 1. Désactiver CSRF
         .csrf(csrf -> csrf.disable())
 
